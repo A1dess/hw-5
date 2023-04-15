89b31daf4df0222605eecde550a1f7c012074780
A1dess <aidosberdibaev.ru@gmail.com>
2023-04-15 18:06:00 +0600

    aidos

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+export function User ({userInfo, getOneUser}) {
+
+
+    return (
+        <div>
+           
+             <span>{userInfo.name}</span>
+            <button onClick={() => getOneUser(userInfo.id)}>more info</button>
+            <p>-----------------------------------</p>
+        </div>
+    )
+}
